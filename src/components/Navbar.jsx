73a29b3cc8478d80c6f3cf27c901e8c896668143
Joaquín Carré <joa_carre21@hotmail.com>
2023-08-17 21:46:22 -0300

    Mejorando apartado de proyectos y agregando los que tengo hasta ahora

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
 
     return (
         <div className="navbar">
-            <img src="/src/assets/logoJC.png" alt="Logo Portfolio" width={40} height={40} className="logo-navbar" />
+            <img src="/src/assets/logoJC.png" alt="Logo Portfolio" width={45} className="logo-navbar" />
             <ul className="navbar-menu">
                 <li><a href="#about_me" className="text-white">Sobre mi</a></li>
                 <li><a href="#projects" className="text-white">Proyectos</a></li>
