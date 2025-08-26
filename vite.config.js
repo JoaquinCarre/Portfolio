a888672a17cf661e522f45d54e2b276dd350cfcb
Joaquín Carré <joa.carre21@gmail.com>
2025-08-26 14:27:26 -0300

    Arreglando problemas para visualizar en ghPages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // 👈 coincide con el nombre exacto de tu repo
   build: {
     outDir: 'docs'
   }
