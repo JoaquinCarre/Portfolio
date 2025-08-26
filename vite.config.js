144ddba2abff56351d5f72b58e64a2135df8ba1b
Joaquín Carré <joa.carre21@gmail.com>
2025-08-26 14:06:00 -0300

    Agregando carpeta docs al hacer build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MI-PORTFOLIO/', // 👈 poné acá el nombre de tu repo
+  build: {
+    outDir: 'docs'
+  }
 })
 
