04deb638e91d349995c6dd9ae2ee8b88bac9a9f8
Joaquín Carré <joa.carre21@gmail.com>
2025-08-26 13:41:01 -0300

    Probando herramientas para que la web funcione desde github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio/', // 👈 muy importante: nombre exacto del repo entre barras. Más adelante, si pongo un dominio propio (ej: midominio.com), puedo volver a dejarlo en / ó borrar esta propiedad.
 })
+
