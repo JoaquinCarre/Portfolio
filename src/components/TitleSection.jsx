3e36f61c22c9884e2ca5fb8c20d141cf26f196c7
Joaquín Carré <joa.carre21@gmail.com>
2025-08-29 21:25:17 -0300

    Subiendo ultimos detalles para seccion de proyectos en su version mobile

--- src/components/TitleSection.jsx
+++ src/components/TitleSection.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 
 const TitleSection = ({ backgroundColor, icon, title }) => {
-    console.log(backgroundColor);
-    console.log(icon);
-    console.log(title);
     return (
         <div className="grid-block" style={{ backgroundColor }}>
         <div className="block-content">
